8563c96db7139a42df481b3152e38b67b9666f40
jOn3th19 <jonethrudio@gmail.com>
2021-09-02 20:04:25 +0800

    created Task.js

---
+++ components/Task.js
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+import React from 'react';
+
+const Task = () => {
+  return (
+    <View>
+      <Text>task</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default Task;
